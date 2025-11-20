7830b778dfe3de6bfa8236142bfd13b69297845e
mohamadbachrouche <mohamadbachrouche@outlook.com>
2025-11-20 10:29:43 +0100

    Implement vehicle ID retrieval in berechneMaut method with secure SQL query

--- javasrc/de/htwberlin/dbtech/aufgaben/ue03/MautServiceImpl.java
+++ javasrc/de/htwberlin/dbtech/aufgaben/ue03/MautServiceImpl.java
@@ -7,6 +7,9 @@ import de.htwberlin.dbtech.exceptions.DataException;
 import de.htwberlin.dbtech.exceptions.AlreadyCruisedException;
 import de.htwberlin.dbtech.exceptions.InvalidVehicleDataException;
 import de.htwberlin.dbtech.exceptions.UnkownVehicleException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Die Klasse realisiert den AusleiheService.
@@ -30,11 +33,37 @@ public class MautServiceImpl implements IMautService {
 		return connection;
 	}
 
-	@Override
-	public void berechneMaut(int mautAbschnitt, int achszahl, String kennzeichen)
-			throws UnkownVehicleException, InvalidVehicleDataException, AlreadyCruisedException {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public void berechneMaut(int mautAbschnitt, int achszahl, String kennzeichen)
+            throws UnkownVehicleException, InvalidVehicleDataException, AlreadyCruisedException {
+
+        // Variable für die Fahrzeug-ID (brauchen wir später für weitere Schritte)
+        Integer fahrzeugId = null;
+
+        // ---------------------------------------------------------
+        // SCHRITT 1: Ist das Fahrzeug bekannt?
+        // ---------------------------------------------------------
+        String sqlCheckVehicle = "SELECT FZ_ID FROM FAHRZEUG WHERE KENNZEICHEN = ?";
+
+        try (PreparedStatement pstmt = getConnection().prepareStatement(sqlCheckVehicle)) {
+            // Das Kennzeichen aus den Methodenparametern setzen
+            pstmt.setString(1, kennzeichen);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    // JA: Das Fahrzeug ist bekannt. Wir merken uns die ID.
+                    fahrzeugId = rs.getInt("FZ_ID");
+                } else {
+                    // NEIN: Das Fahrzeug wurde nicht gefunden -> Exception werfen
+                    throw new UnkownVehicleException("Fahrzeug mit Kennzeichen " + kennzeichen + " ist unbekannt.");
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataException(e);
+        }
+
+        // TODO: Hier geht es weiter mit Schritt 2: Achszahl prüfen...
+    }
 
 
 
